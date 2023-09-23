cab8954b7447037704ee8fe3cafa49364a76065f
Donald Chow <donald7290@gmail.com>
2023-09-23 23:46:23 +0900

    Night time changes

--- frontend/src/components/ResultChart.vue
+++ frontend/src/components/ResultChart.vue
@@ -1,39 +1,75 @@
 <template>
-  <h1>Hi I am result</h1>
-  <div style="width: 750px; height: 750px; background:url(https://i.stack.imgur.com/Y1mnB.png)">
+  <h1>Result Somatograph</h1>
+  <div class="background">
     <canvas id="somatograph" width="750" height="750"></canvas>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import Chart from 'chart.js/auto'
 import { onMounted } from 'vue';
+import 'chartjs-plugin-datalabels';
+
+const althelete = ref([
+  {
+    "type": "Kayaking",
+    "mesomorphy": 3.82,
+    "endomorphy": 3.38,
+    "ectomorphy": 3.87
+  },
+  {
+    "type": "Basketball",
+    "mesomorphy": 4.09,
+    "endomorphy": 2.77,
+    "ectomorphy": 3
+  },
+  {
+    "type": "Boxing",
+    "mesomorphy": 3.7,
+    "endomorphy": 2.3,
+    "ectomorphy": 2.3
+  },
+  {
+    "type": "Football",
+    "mesomorphy": 3.57,
+    "endomorphy": 2.64,
+    "ectomorphy": 3
+  }
+])
 
 onMounted(() => {
 
   const result =
-            {'Ectomorphy': 4,
-            'Mesomorphy': 1.344,
-            'Endomorphy': 4.6 }
-  const volleyball =
-            {'Ectomorphy': 3.4,
-            'Mesomorphy': 2.7,
-            'Endomorphy': 2.9 }
-  const basketball =
-            {'Ectomorphy': 3.7,
-            'Mesomorphy': 2.7,
-            'Endomorphy': 2.0 }
-  const handball =
-            {'Ectomorphy': 4.2,
-            'Mesomorphy': 4.7,
-            'Endomorphy': 1.8 }
+  {
+    'ectomorphy': 4,
+    'mesomorphy': 1.344,
+    'endomorphy': 4.6
+  }
+  // const volleyball =
+  // {
+  //   'Ectomorphy': 3.4,
+  //   'Mesomorphy': 2.7,
+  //   'Endomorphy': 2.9
+  // }
+  // const basketball =
+  // {
+  //   'Ectomorphy': 3.7,
+  //   'Mesomorphy': 2.7,
+  //   'Endomorphy': 2.0
+  // }
+  // const handball =
+  // {
+  //   'Ectomorphy': 4.2,
+  //   'Mesomorphy': 4.7,
+  //   'Endomorphy': 1.8
+  // }
 
   function plotResult(result: Array) {
-    const x = result['Ectomorphy'] - result['Endomorphy']
-    const y = 2 * result['Mesomorphy'] - (result['Endomorphy'] + result['Ectomorphy'])
-    console.log({x, y});
-
-    return {x, y}
+    const x = result['ectomorphy'] - result['endomorphy']
+    const y = 2 * result['mesomorphy'] - (result['endomorphy'] + result['ectomorphy'])
+    console.log({ x, y });
+    return { x, y }
   }
 
   const data = {
@@ -43,55 +79,16 @@ onMounted(() => {
       backgroundColor: 'rgb(255, 99, 132)'
     },
     {
-    label: 'Altheletes',
-    data: [plotResult(volleyball), plotResult(basketball), plotResult(handball)],
-    backgroundColor: 'rgb(0, 0, 255)',
-    datalabels: {
-                align: 'end', // Position of the label relative to the point
-                anchor: 'end', // Anchor point of the label
-                font: {
-                    weight: 'bold' // Label font weight
-                },
-                formatter: function(value, context) {
-                    return context.dataset.data[context.dataIndex].label; // Display the label for each point
-                }}
-  }],
+      data: althelete.value.map(item => ({
+        label: item.type,
+        x: plotResult(item).x,
+        y: plotResult(item).y
+      })),
+      backgroundColor: 'rgb(0, 0, 255)'
+    }],
   };
 
-  // const image = new Image();
-  // image.src = 'https://i.stack.imgur.com/Y1mnB.png';
-
-  // const plugin = {
-  //       id: 'customCanvasBackgroundImage',
-  //       beforeDraw: (chart) => {
-  //         if (image.complete) {
-  //           const ctx = chart.ctx;
-  //           const { top, left, width, height } = chart.chartArea;
-
-  //           // Calculate the dimensions to fit the entire chart area
-  //           const aspectRatio = image.width / image.height;
-  //           const chartAspectRatio = width / height;
-
-  //           let imgWidth, imgHeight;
-
-  //           if (aspectRatio > chartAspectRatio) {
-  //             imgWidth = width;
-  //             imgHeight = width / aspectRatio;
-  //           } else {
-  //             imgWidth = height * aspectRatio;
-  //             imgHeight = height;
-  //           }
-
-  //           const x = left + width / 2 - imgWidth / 2;
-  //           const y = top + height / 2 - imgHeight / 2;
-
-  //           ctx.drawImage(image, x, y, imgWidth, imgHeight);
-  //         } else {
-  //           image.onload = () => chart.draw();
-  //         }
-  //       },
-  //     };
-
+  console.log(data);
   new Chart(
     document.getElementById('somatograph'),
     {
@@ -99,20 +96,42 @@ onMounted(() => {
       data: data,
       options: {
         scales: {
-                x: {
-                    type: 'linear',
-                    position: 'bottom',
-                    min: -8, // Set the minimum value for the x-axis
-                    max: 9,  // Set the maximum value for the x-axis
-                },
-                y: {
-                    min: -11, // Set the minimum value for the y-axis
-                    max: 16,  // Set the maximum value for the y-axis
-                }
-            }
+          x: {
+            type: 'linear',
+            position: 'bottom',
+            min: -8, // Set the minimum value for the x-axis
+            max: 9,  // Set the maximum value for the x-axis
+          },
+          y: {
+            min: -11, // Set the minimum value for the y-axis
+            max: 16,  // Set the maximum value for the y-axis
+          }
+        }
       },
-      // plugins: [plugin]
+      plugins: {
+        datalabels: {
+          align: 'end', // Position of the label relative to the point
+          anchor: 'end', // Anchor point of the label
+          font: {
+            weight: 'bold' // Label font weight
+          },
+          formatter: function (value, context) {
+            return context.dataset.data[context.dataIndex].label; // Display the label for each point
+          }
+        }
+      }
     }
-    );
-  })
+  );
+})
 </script>
+
+<style>
+.background {
+  background-size: 101%;
+  background-position: top 5px right -15px;
+  width: 750px;
+  height: 750px;
+  background-image: url(https://i.stack.imgur.com/Y1mnB.png);
+  /* background-image: url(~@/assets/somatograph.png) */
+}
+</style>
--- frontend/src/components/SubmitForm.vue
+++ frontend/src/components/SubmitForm.vue
@@ -1,48 +1,72 @@
 <template>
   <form action="" class="form" @submit.prevent="createScan">
-    <div class="flex">
+    <h1 class="font-bold">Input your data</h1>
+    <div class="grid grid-cols-3">
       <div class="input-wrapper">
-        <label for="name">Name</label>
+        <label for="name">Name: </label>
         <input name='name' id="name" type="text" v-model="formData.name">
       </div>
       <div class="input-wrapper  px-4">
-        <label for="age">Age</label>
+        <label for="age">Age: </label>
         <input name='age' id="age" type="number" v-model="formData.age">
       </div>
-
-    </div>
-    <div class="flex">
       <div class="input-wrapper">
-        <label for="height">Height(cm)</label>
-        <input name='height' id="height" type="number" v-model="formData.height">
-      </div>
-      <div class="input-wrapper px-4">
-        <label for="weight">Weight(kg)</label>
-        <input name='weight' id="weight" type="number" v-model="formData.weight">
-      </div>
-      <div class="input-wrapper">
-        <label for="gender">Gender</label>
+        <label for="gender">Gender: </label>
         <select name='gender' id="gender" v-model="formData.gender">
           <option value="male">Male</option>
           <option value="female">Female</option>
         </select>
       </div>
     </div>
-
-    <div class="input-wrapper">
-      <label for="frontPhoto">Front Photo</label>
-      <input name='frontPhoto' id="frontPhoto" type="file" @change="handleImageUpload('frontImage', $event)" accept="image/*">
+    <div class="grid grid-cols-3">
+      <div class="input-wrapper">
+        <label for="height">Height(cm): </label>
+        <input name='height' id="height" type="number" v-model="formData.height">
+      </div>
+      <div class="input-wrapper px-4">
+        <label for="weight">Weight(kg): </label>
+        <input name='weight' id="weight" type="number" v-model="formData.weight">
+      </div>
     </div>
-    <div class="input-wrapper">
-      <label for="rightPhoto">Side Photo</label>
-      <input name='rightPhoto' id="rightPhoto" type="file" @change="handleImageUpload('rightImage', $event)" accept="image/*">
+    <h1 class="font-semibold">Skinfold Measurement</h1>
+    <div class="grid grid-cols-3">
+      <div class="input-wrapper">
+        <label for="height">Triceps: </label>
+        <input name='height' id="height" type="number" v-model="formData.height">
+      </div>
+      <div class="input-wrapper px-4">
+        <label for="weight">Subscapular: </label>
+        <input name='weight' id="weight" type="number" v-model="formData.weight">
+      </div>
+    </div>
+    <div class="grid grid-cols-3">
+      <div class="input-wrapper">
+        <label for="height">Supraspinale: </label>
+        <input name='height' id="height" type="number" v-model="formData.height">
+      </div>
+      <div class="input-wrapper px-4">
+        <label for="weight">Calf?: </label>
+        <input name='weight' id="weight" type="number" v-model="formData.weight">
+      </div>
+    </div>
+    <div class="grid grid-cols-2">
+      <div class="input-wrapper">
+        <label for="frontPhoto">Front Photo: </label>
+        <input name='frontPhoto' id="frontPhoto" type="file" @change="handleImageUpload('frontImage', $event)"
+          accept="image/*">
+      </div>
+      <div class="input-wrapper">
+        <label for="rightPhoto">Side Photo: </label>
+        <input name='rightPhoto' id="rightPhoto" type="file" @change="handleImageUpload('rightImage', $event)"
+          accept="image/*">
+      </div>
     </div>
     <button type="submit" class="button">submit</button>
   </form>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
+import { ref } from 'vue'
 import axios from 'axios';
 const formData = ref({
   name: '',
@@ -68,7 +92,13 @@ function handleImageUpload(fieldName: string, event: Event) {
 
 function createScan() {
   const data = {
-    user: formData.value,
+    user: {
+      name: formData.value.name,
+      height: formData.value.height * 10,
+      weight: formData.value.weight * 1000,
+      age: formData.value.age,
+      gender: formData.value.gender
+    },
     front_image: frontImage.value,
     right_image: rightImage.value
   }
@@ -81,36 +111,46 @@ function createScan() {
     .catch((error) => {
       console.error('Error:', error);
     });
-  }
+}
 
 
 </script>
 
 <style scoped>
-  .form {
-    width: 80%;
-    margin: auto;
-    background-color: beige;
-    padding: 16px;
-    border-radius: 8px;
-    box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.1);
-  }
+.form {
+  width: 80%;
+  margin: auto;
+  background-color: beige;
+  padding: 16px;
+  border-radius: 8px;
+  box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.1);
+}
 
-  .input-wrapper {
-      margin-bottom: 16px;
-      display: flex;
-      justify-content: space-between;
-    }
+input {
+  border-radius: 5px;
+  padding: 1px 2px;
+}
 
-  .input-wrapper>label {
-    min-width: 50px;
-  }
+input:focus {
+  box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.1);
+}
 
-  .button {
-    background-color: blue;
-    border-radius: 8px;
-    padding:  8px 16px;
-    color: white;
-    box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.1);
-  }
+.input-wrapper {
+  margin-bottom: 16px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.input-wrapper>label {
+  min-width: 50px;
+  padding-right: 8px;
+}
+
+.button {
+  background-color: blue;
+  border-radius: 8px;
+  padding: 8px 16px;
+  color: white;
+  box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.1);
+}
 </style>
